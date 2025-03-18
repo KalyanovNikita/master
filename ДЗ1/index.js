396da0b651649fc9990e3fe903e90078ad3a21f9
KalyanovNikita <costner@bk.ru>
2025-03-18 21:08:57 +0300

    Домашнее задание 1 и 2

---
+++ ДЗ1/index.js
@@ -0,0 +1,56 @@
+// console.log("Hello World!");
+
+const age = 34;
+
+let userName = "nikita";
+
+let audi = true;
+
+let bmw = null;
+
+let userAdmin;
+
+const admin = {
+  name: "nikita",
+  age: 34,
+};
+
+let mercedes = comfort("rich");
+
+let number = 12345678901234567890n;
+
+lowCar = "Maserati";
+
+let bigCar = { name: "Dodge" };
+
+object.monkey = "Champanzee";
+
+object.name = { name: "Boeing" };
+
+var superCar = { name: "Audi" };
+
+luser.car = "bmw";
+
+richCar = { name: "mercedes" };
+
+// console.log(age);
+// console.log(userName);
+// console.log(audi);
+// console.log(bmw);
+// console.log(userAdmin);
+// console.log(admin);
+// console.log(mercedes);
+// console.log(number);
+// console.log(typeof age);
+// console.log(typeof userName);
+// console.log(typeof audi);
+// console.log(typeof bmw);
+// console.log(typeof userAdmin);
+// console.log(typeof admin);
+// console.log(typeof mercedes);
+// console.log(typeof number);
+// console.log(lowCar);
+// console.log(object.monkey);
+// console.log(object.name);
+// console.log(luser.car);
+// console.log(luser.car);
